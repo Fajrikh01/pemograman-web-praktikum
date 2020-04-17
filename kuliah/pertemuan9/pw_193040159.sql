3423877addbbbf93d587d3759bd6557be39d5346
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2020-04-17 08:14:19 +0700

    Update pw_193040159.sql

--- kuliah/pertemuan9/pw_193040159.sql
+++ kuliah/pertemuan9/pw_193040159.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Apr 2020 pada 09.48
+-- Waktu pembuatan: 17 Apr 2020 pada 03.12
 -- Versi server: 10.1.37-MariaDB
 -- Versi PHP: 7.3.0
 
@@ -21,38 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `pw_193040159`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `alat_musik`
---
-
-CREATE TABLE `alat_musik` (
-  `id` int(11) NOT NULL,
-  `gambar` varchar(100) NOT NULL,
-  `nama_alat_musik` varchar(100) NOT NULL,
-  `provinsi` varchar(100) NOT NULL,
-  `sumber_bunyi` varchar(100) NOT NULL,
-  `cara_memainkan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `alat_musik`
---
-
-INSERT INTO `alat_musik` (`id`, `gambar`, `nama_alat_musik`, `provinsi`, `sumber_bunyi`, `cara_memainkan`) VALUES
-(1, 'angklung.jpg', 'Angklung', 'Jawa Barat', 'Idiofon', 'Dimainkan dengan cara digoyangkan'),
-(2, 'sasando.jpg', 'Sasando', 'Nusa Tenggara Timur', 'Kordofon', 'Dimainkan dengan cara dipetik seperti gitar'),
-(3, 'tifa.jpg', 'Tifa', 'Maluku dan Papua', 'Membranfon', 'Dimainkan dengan cara dipukul'),
-(4, 'aramba.jpg', 'Aramba', 'Sumatera Utara', 'Ideofon', 'dipukul dengan menggunakan pemukul khusus'),
-(5, 'gambus.jpg', 'Gambus', 'Riau', 'Kordofon', 'Dipetik dengan menggunakan jari'),
-(6, 'bonang.jpg', 'Bonang', 'Jawa Timur', 'Ideofon', 'Dimainkan dengan cara dipukul'),
-(7, 'panting.jpg', 'Panting', 'Kalimantan Selatan', 'Kordofon', 'Dimainkan dengan cara dipetik bagian senarnya'),
-(8, 'kacapi.jpg', 'Kecapi', 'Sulawesi Barat', 'Kordofon', 'Dimainkan dengan cara dipetik'),
-(9, 'gong.jpg', 'Gong', 'Jawa Barat', 'Membranfon', 'Dipukul menggunakan alat pukul yang didesain secara khusus'),
-(10, 'genggong.jpg', 'Genggong', 'Sumatera Selatan', 'Aerofon', 'Dimainkan dengan cara ditiup'),
-(11, 'kompang.jpg', 'Kompang', 'Lampung', 'Kordofon', 'Dimainkan dengan cara dipukul');
 
 -- --------------------------------------------------------
 
@@ -90,12 +58,6 @@ INSERT INTO `mahasiswa` (`id`, `nama`, `nrp`, `email`, `jurusan`, `gambar`) VALU
 --
 
 --
--- Indeks untuk tabel `alat_musik`
---
-ALTER TABLE `alat_musik`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indeks untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
@@ -104,12 +66,6 @@ ALTER TABLE `mahasiswa`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
-
---
--- AUTO_INCREMENT untuk tabel `alat_musik`
---
-ALTER TABLE `alat_musik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `mahasiswa`

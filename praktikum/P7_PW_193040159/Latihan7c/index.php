6b756f18a6672fefc0725ca864bc43e66f1e3c22
Fajrikh01 <59136617+Fajrikh01@users.noreply.github.com>
2020-05-08 16:27:10 +0700

    update

--- praktikum/P7_PW_193040159/Latihan7c/index.php
+++ praktikum/P7_PW_193040159/Latihan7c/index.php
@@ -2,7 +2,16 @@
 
 require 'php/functions.php';
 
-$alat_musik = query("SELECT * FROM alat_musik");
+if (isset($_GET['cari'])) {
+    $keyword = $_GET['keyword'];
+    $alat_musik = query("SELECT * FROM alat_musik WHERE
+                   nama_alat_musik LIKE '%$keyword%' OR
+                   provinsi LIKE '%$keyword%' OR
+                   sumber_bunyi LIKE '%$keyword%' OR
+                   cara_memainkan LIKE '%$keyword%'");
+} else {
+    $alat_musik = query("SELECT * FROM alat_musik");
+}
 
 ?>
 
@@ -20,6 +29,19 @@ $alat_musik = query("SELECT * FROM alat_musik");
     <div id="page-wrap">
         <div class="content">
             <h1 class="judul">Daftar Nama Alat Musik</h1>
+            <form action="" method="GET">
+                <input type="text" name="keyword" size="20" autocomplete="off" autofocus>
+                <button type="submit" name="cari" style="width: auto">Cari</button>
+            </form>
+
+            <?php if (empty($alat_musik)) : ?>
+                <tr>
+                    <td colspan="7">
+                        <p style="color: red; font-style:italic">data tidak ditemukan!</p>
+                    </td>
+                </tr>
+            <?php endif; ?>
+
             <?php foreach ($alat_musik as $am) : ?>
                 <div class="kotak">
                     <p class="nama">
